2a0bdfe863f66c621b62f17209765c2300e64bfe
cbclemmer <coltonclemmerdev@gmail.com>
2024-03-23 15:09:35 -0500

    Add id to references

--- reference.py
+++ reference.py
@@ -1,11 +1,14 @@
 class Reference:
     def __init__(self, data: object):
+        self.id = None
         self.title = None
         self.arxiv_id = None
         self.url = None
         self.author = None
         self.data = data
 
+        if 'ID' in data:
+            self.id = data["ID"]
         if 'arxiv_id' in data:
             self.arxiv_id = data["arxiv_id"]
         if 'url' in data:
@@ -17,6 +20,7 @@ class Reference:
 
     def to_obj(self):
         return {
+            "id": self.id,
             "title": self.title,
             "arxiv_id": self.arxiv_id,
             "url": self.url,
